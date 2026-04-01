ca01ae1f2577c8f3b56ea8551b98083e979f69e9
manohar-1989 <manohardhumal1998@gmail.com>
2026-04-01 13:28:32 +0530

    Added core business logic

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿function calculateWorkSuccess(effort, productivity) {
+  const result = effort * productivity;
+  console.log("Team productivity score:", result);
+  return result;
+}
+
+// Adding 20 more lines for the demo stats...
+for (let i = 0; i < 20; i++) {
+  console.log("Analyzing data point " + i);
+}
